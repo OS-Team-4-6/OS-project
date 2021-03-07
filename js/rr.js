db46779c7a1db5b393b98518b02e3489aee12fdd
nehabadiani <nehabadiani60@gmail.com>
2021-03-07 18:04:39 +0530

    update rr

--- js/rr.js
+++ js/rr.js
@@ -1,98 +1,126 @@
 function rr(pt, at, bt, n, timeQuantum) {
-    n -= 1;
-    var count, 
-        time, 
-        remain, 
-        flag=0,
-        temp = 0,  
-        waitTime=0, 
-        turnAroundTime=0;
-    let rt = [],
-        process = [],
-        tat = [],
-        wt = [];
-        arrivalTime = [],
-        burstTime = [];
+  n -= 1;
+  var count,
+    time,
+    remain,
+    flag = 0,
+    temp = 0,
+    waitTime = 0,
+    turnAroundTime = 0;
+  let rt = [],
+    process = [],
+    tat = [],
+    wt = [];
+  (arrivalTime = []), (burstTime = []);
 
-    remain = n;
-    for(count = 0; count < n; count++)
-        rt[count] = bt[count];
-    for(time = 0, count = 0; remain != 0;) {
-        if(rt[count] <= timeQuantum && rt[count] > 0) { 
-            time += rt[count]; 
-            rt[count] = 0; 
-            flag = 1; 
-        } 
-        else if(rt[count] > 0) { 
-            rt[count] -= timeQuantum; 
-            time += timeQuantum; 
-        } 
-        if(rt[count] == 0 && flag == 1) { 
-            remain--; 
-            process.push(count + 1);
-            tat.push(time - at[count]);
-            wt.push(time - at[count] - bt[count]); 
-            waitTime += time - at[count] - bt[count]; 
-            turnAroundTime += time - at[count]; 
-            flag = 0; 
-        } 
-        if(count == n-1) 
-            count = 0; 
-        else if(at[count + 1] <= time) 
-            count++; 
-        else 
-            count = 0; 
+  remain = n;
+  for (count = 0; count < n; count++) rt[count] = bt[count];
+  for (time = 0, count = 0; remain != 0; ) {
+    if (rt[count] <= timeQuantum && rt[count] > 0) {
+      time += rt[count];
+      rt[count] = 0;
+      flag = 1;
+    } else if (rt[count] > 0) {
+      rt[count] -= timeQuantum;
+      time += timeQuantum;
     }
-    for(count = 0; count < n; count++) {
-        arrivalTime.push(at[process[count]-1]);
-        burstTime.push(bt[process[count]-1]);
+    if (rt[count] == 0 && flag == 1) {
+      remain--;
+      process.push(count + 1);
+      tat.push(time - at[count]);
+      wt.push(time - at[count] - bt[count]);
+      waitTime += time - at[count] - bt[count];
+      turnAroundTime += time - at[count];
+      flag = 0;
     }
-    // Backend ends here
-    document.getElementById('att').innerHTML = "Average Turn-around Time : " + turnAroundTime/n ;
-    document.getElementById('awt').innerHTML = "Average Waiting Time : " + waitTime/n ;
-    
-    var Table = document.getElementById("rrTable");
-    var rows = Table.querySelectorAll('tr');
-    for(let i = 1; i < rows.length; i++) {
-        rows[i].children[0].innerHTML = process[i-1];
-        rows[i].children[1].innerHTML = '<input id="rat'+ i +'" placeholder="at'+ i +'" value='+arrivalTime[i-1]+'>';
-        rows[i].children[2].innerHTML = '<input id="rbt'+ i +'" placeholder="bt'+ i +'" value='+burstTime[i-1]+'>';
-        // rows[i].children[3].innerHTML = finish[i-1];
-        rows[i].children[4].innerHTML = tat[i-1];
-        rows[i].children[5].innerHTML = wt[i-1];
-    }
+    if (count == n - 1) count = 0;
+    else if (at[count + 1] <= time) count++;
+    else count = 0;
+  }
+  for (count = 0; count < n; count++) {
+    arrivalTime.push(at[process[count] - 1]);
+    burstTime.push(bt[process[count] - 1]);
+  }
+  // Backend ends here
+  document.getElementById("att").innerHTML =
+    "Average Turn-around Time : " + turnAroundTime / n;
+  document.getElementById("awt").innerHTML =
+    "Average Waiting Time : " + waitTime / n;
+
+  var Table = document.getElementById("rrTable");
+  var rows = Table.querySelectorAll("tr");
+  for (let i = 1; i < rows.length; i++) {
+    rows[i].children[0].innerHTML = process[i - 1];
+    rows[i].children[1].innerHTML =
+      '<input id="rat' +
+      i +
+      '" placeholder="at' +
+      i +
+      '" value=' +
+      arrivalTime[i - 1] +
+      ">";
+    rows[i].children[2].innerHTML =
+      '<input id="rbt' +
+      i +
+      '" placeholder="bt' +
+      i +
+      '" value=' +
+      burstTime[i - 1] +
+      ">";
+    //rows[i].children[3].innerHTML = finish[i - 1];
+    rows[i].children[4].innerHTML = tat[i - 1];
+    rows[i].children[5].innerHTML = wt[i - 1];
+  }
 }
 
 var RRcounter = 2;
 function RRfn() {
-    let atimes = [];
-    let btimes = [];
-    let ptimes = [];
-    for (let i = 1; i < RRcounter; i++) {
-        if (document.getElementById('rat' + i) !== undefined && document.getElementById('rbt' + i) !== undefined) {
-            ptimes.push(parseInt(document.getElementById("rpid" + i).value));
-            atimes.push(parseInt(document.getElementById("rat" + (i)).value));
-            btimes.push(parseInt(document.getElementById("rbt" + (i)).value));
-        }
+  let atimes = [];
+  let btimes = [];
+  let ptimes = [];
+  for (let i = 1; i < RRcounter; i++) {
+    if (
+      document.getElementById("rat" + i) !== undefined &&
+      document.getElementById("rbt" + i) !== undefined
+    ) {
+      ptimes.push(parseInt(document.getElementById("rpid" + i).value));
+      atimes.push(parseInt(document.getElementById("rat" + i).value));
+      btimes.push(parseInt(document.getElementById("rbt" + i).value));
     }
-    let timeQuantum = parseInt(document.getElementById('tq').value);
-    rr(ptimes, atimes, btimes, RRcounter, timeQuantum);
+  }
+  let timeQuantum = parseInt(document.getElementById("tq").value);
+  rr(ptimes, atimes, btimes, RRcounter, timeQuantum);
 }
 
 function addRRRow() {
-    $('#rrTable').append('<tr><td><input disabled type="number" id="rpid' + RRcounter + '" placeholder="pid' + RRcounter + '" value="' + RRcounter + '" /></td><td><input type="number" id="rat' + RRcounter + '" placeholder="at' + RRcounter + '" /></td><td><input type="number" id="rbt' + RRcounter + '" placeholder="bt' + RRcounter + '" /></td><td></td><td></td><td></td></tr>');
-    RRcounter += 1;
+  $("#rrTable").append(
+    '<tr><td><input disabled type="number" id="rpid' +
+      RRcounter +
+      '" placeholder="pid' +
+      RRcounter +
+      '" value="' +
+      RRcounter +
+      '" /></td><td><input type="number" id="rat' +
+      RRcounter +
+      '" placeholder="at' +
+      RRcounter +
+      '" /></td><td><input type="number" id="rbt' +
+      RRcounter +
+      '" placeholder="bt' +
+      RRcounter +
+      '" /></td><td></td><td></td><td></td></tr>'
+  );
+  RRcounter += 1;
 }
-  
-function deleteRRRow() {
-    var table = document.getElementById("rrTable");
-    var rowCount = table.rows.length;
 
-    if (rowCount > 2) {
-        table.deleteRow(rowCount - 1);
-        RRcounter -= 1;
-    }
-    else {
-        alert("Cannot delete row");
-    }
+function deleteRRRow() {
+  var table = document.getElementById("rrTable");
+  var rowCount = table.rows.length;
+
+  if (rowCount > 2) {
+    table.deleteRow(rowCount - 1);
+    RRcounter -= 1;
+  } else {
+    alert("Cannot delete row");
+  }
 }
--- rr.php
+++ rr.php
@@ -1,60 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SA | Round Robin</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/algo.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SA | Round Robin</title>
+        <link rel="stylesheet" href="./css/style.css">
+        <link rel="stylesheet" href="./css/algo.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    </head>
 
-<body>
-    <div class="main">
-        <div class="table-con">
-            <div class="table">
-                <table id="rrTable" class="nameTable">
-                    <tr>
-                        <th>Process ID</th>
-                        <th>Arrival Time</th>
-                        <th>Burst Time</th>
-                        <th>Completion Time</th>
-                        <th>Turn Around Time</th>
-                        <th>Waiting Time</th>
-                    </tr>
-                    <tr>
-                        <td><input disabled type="number" id="rpid1" placeholder="pid1" value="1"></td>
-                        <td><input type="number" id="rat1" placeholder="at1"></td>
-                        <td><input type="number" id="rbt1" placeholder="bt1"></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </table>
-            </div>
-            <div class='table-action-grp'>
-                <div><button onclick="addRRRow()">
-                        <div><i class="fas fa-plus-circle"></i></div>
-                        <div>Add Row</div>
-                    </button></div>
-                <div><button id='deleteBtn' onclick="deleteRRRow()">
-                        <i class="fas fa-times"></i>
-                    </button></div>
-            </div>
-            <div class='submit-div'>
-                <button onclick="RRfn()" id="btnFcfs" class="submit-btn">
-                    Submit
-                </button>
-            </div>
-            <h3>Time Quantum: </h3><input id="tq" placeholder="tq">
-            <div class="avg">
-                <div id="att"> </div>
-                <div id="awt"> </div>
+    <body>
+        <div class="main">
+            <div class="table-con">
+                <div class="table">
+                    <table id="rrTable" class="nameTable">
+                        <tr>
+                            <th>Process ID</th>
+                            <th>Arrival Time</th>
+                            <th>Burst Time</th>
+                            <th>Completion Time</th>
+                            <th>Turn Around Time</th>
+                            <th>Waiting Time</th>
+                        </tr>
+                        <tr>
+                            <td><input disabled type="number" id="rpid1" placeholder="pid1" value="1"></td>
+                            <td><input type="number" id="rat1" placeholder="at1"></td>
+                            <td><input type="number" id="rbt1" placeholder="bt1"></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+                <div class='table-action-grp'>
+                    <div><button onclick="addRRRow()">
+                            <div><i class="fas fa-plus-circle"></i></div>
+                            <div>Add Row</div>
+                        </button></div>
+                    <div><button id='deleteBtn' onclick="deleteRRRow()">
+                            <i class="fas fa-times"></i>
+                        </button></div>
+                </div>
+                <div class='submit-div'>
+                    <button onclick="RRfn()" id="btnFcfs" class="submit-btn">
+                        Submit
+                    </button>
+                </div>
+                <h3>Time Quantum: </h3><input id="tq" placeholder="tq">
+                <div class="avg">
+                    <div id="att"> </div>
+                    <div id="awt"> </div>
+                </div>
             </div>
         </div>
-    </div>
-    <script src="./js/rr.js"></script>
-    <script defer src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" integrity="sha384-9/D4ECZvKMVEJ9Bhr3ZnUAF+Ahlagp1cyPC7h5yDlZdXs4DQ/vRftzfd+2uFUuqS" crossorigin="anonymous"></script>
-</body>
+        <script src="./js/rr.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"
+            integrity="sha384-9/D4ECZvKMVEJ9Bhr3ZnUAF+Ahlagp1cyPC7h5yDlZdXs4DQ/vRftzfd+2uFUuqS" crossorigin="anonymous">
+        </script>
+    </body>
+
 </html>
