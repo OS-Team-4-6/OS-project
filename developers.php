be8c7f779edb785f0aec5cd5dc94ae4a225ba257
nehabadiani <nehabadiani60@gmail.com>
2021-03-21 18:33:08 +0530

    add developers page

--- developers.php
+++ developers.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.css" />
-    <link rel="stylesheet" href="./css/tabs.css" />
-    <link rel="stylesheet" href="./css/person-card.css" />
-    <title>Developers</title>
-    <script src="./js/tabs.js"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.css" />
+        <link rel="stylesheet" href="./css/tabs.css" />
+        <link rel="stylesheet" href="./css/person-card.css" />
+        <title>Developers</title>
+        <script src="./js/tabs.js"></script>
+    </head>
 
-<body>
-    <?php
+    <body>
+        <?php
     $title_1 = "";
     $title_2 = "Developers";
     $main_desc = "";
@@ -21,179 +21,179 @@
     include "header.php";
     ?>
 
-    <div class="tabs-container flex-layout ">
-        <div data-tab="disk" class="dev tab h4 active-tab flex-layout justify-center items-center text-uppercase">
-            Disk Scheduling
+        <div class="tabs-container flex-layout ">
+            <div data-tab="disk" class="dev tab h4 active-tab flex-layout justify-center items-center text-uppercase">
+                Disk Scheduling
+            </div>
+            <div data-tab="scheduling" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+                Scheduling
+            </div>
+            <div data-tab="pageRep" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+                Page Replacement
+            </div>
+            <div data-tab="concurrency" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+                Concurrency & Deadlock
+            </div>
         </div>
-        <div data-tab="scheduling" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-            Scheduling
+
+        <div id="disk" class="tab-view">
+            <section class="person-cards">
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Lorem Ipsum</h4>
+                        <h5 class="person-card-des">President</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Lorem Ipsum</h4>
+                        <h5 class="person-card-des">President</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Lorem Ipsum</h4>
+                        <h5 class="person-card-des">President</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Lorem Ipsum</h4>
+                        <h5 class="person-card-des">President</h5>
+                    </div>
+                </div>
+
+            </section>
         </div>
-        <div data-tab="pageRep" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-            Page Replacement
+        <div id="scheduling" class="tab-view">
+            <section class="person-cards">
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/nehaBadiani.JPG" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Neha Badiani</h4>
+                        <h5 class="person-card-des">Team Lead</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/divijShah.jpeg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Divij Shah</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/rajviShah.jpeg" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Rajvi Shah</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/rushabhShah.JPG" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Rushabh Shah</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/vedantPatel.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Vedant Patel</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+            </section>
         </div>
-        <div data-tab="concurrency" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-            Concurrency & Deadlock
+        <div id="pageRep" class="tab-view">
+            <section class="person-cards">
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/smitVekaria.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Smit Vekaria</h4>
+                        <h5 class="person-card-des">Team Lead</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/atharvaPatel.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Atharva Patel</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/maitriSurti.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Maitri Surti</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/shramKadia.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Shram Kadia</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+                <div class="person-card white-text">
+                    <div class="person-card-header">
+                        <img class="person-card-img" src="./assets/images/vyomShah.png" alt="Person Image">
+                    </div>
+                    <div class="person-card-body">
+                        <h4 class="person-card-name">Vyom Shah</h4>
+                        <h5 class="person-card-des">Member</h5>
+                    </div>
+                </div>
+
+            </section>
         </div>
-    </div>
+        <div id="concurrency" class="tab-view">
 
-    <div id="disk" class="tab-view">
-        <section class="person-cards">
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-        </section>
-    </div>
-    <div id="scheduling" class="tab-view">
-        <section class="person-cards">
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-        </section>
-    </div>
-    <div id="pageRep" class="tab-view">
-        <section class="person-cards">
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-            <div class="person-card white-text">
-                <div class="person-card-header">
-                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                </div>
-                <div class="person-card-body">
-                    <h4 class="person-card-name">Lorem Ipsum</h4>
-                    <h5 class="person-card-des">President</h5>
-                </div>
-            </div>
-
-        </section>
-    </div>
-    <div id="concurrency" class="tab-view">
-
-    </div>
-</body>
+        </div>
+    </body>
 
 </html>
--- js/ljf.js
+++ js/ljf.js
@@ -1,140 +1,179 @@
 function LJF(num, mat) {
-    // Arrange Arrival
-    num = num - 1;
-    // console.log(num);
-    for (let i = 0; i < num; i++) {
-        for (let j = 0; j < num - i - 1; j++) {
-            if (mat[j][1] > mat[j + 1][1]) {
-                for (let k = 0; k < 5; k++) {
-                    let temp = mat[j][k];
-                    mat[j][k] = mat[j + 1][k];
-                    mat[j + 1][k] = temp;
-                }
-            }
+  // Arrange Arrival
+  num = num - 1;
+  // console.log(num);
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num - i - 1; j++) {
+      if (mat[j][1] > mat[j + 1][1]) {
+        for (let k = 0; k < 5; k++) {
+          let temp = mat[j][k];
+          mat[j][k] = mat[j + 1][k];
+          mat[j + 1][k] = temp;
         }
+      }
     }
-    // Completion Time
-    let temp = 0,
-        val = -1;
-    mat[0][3] = mat[0][1] + mat[0][2];
-    mat[0][5] = mat[0][3] - mat[0][1];
-    mat[0][4] = mat[0][5] - mat[0][2];
+  }
+  // Completion Time
+  let temp = 0,
+    val = -1;
+  mat[0][3] = mat[0][1] + mat[0][2];
+  mat[0][5] = mat[0][3] - mat[0][1];
+  mat[0][4] = mat[0][5] - mat[0][2];
 
-    for (let i = 1; i < num; i++) {
-        temp = mat[i - 1][3];
-        let low = mat[i][2];
-        for (let j = i; j < num; j++) {
-            if (temp >= mat[j][1] && low >= mat[j][2]) {
-                low = mat[j][2];
-                val = j;
-            }
-        }
-        mat[val][3] = temp + mat[val][2];
-        mat[val][5] = mat[val][3] - mat[val][1];
-        mat[val][4] = mat[val][5] - mat[val][2];
-        for (let k = 0; k < 6; k++) {
-            let tem = mat[val][k];
-            mat[val][k] = mat[i][k];
-            mat[i][k] = tem;
-        }
+  for (let i = 1; i < num; i++) {
+    temp = mat[i - 1][3];
+    let low = mat[i][2];
+    for (let j = i; j < num; j++) {
+      if (temp >= mat[j][1] && low >= mat[j][2]) {
+        low = mat[j][2];
+        val = j;
+      }
     }
-    //BackEnd Ends here
+    mat[val][3] = temp + mat[val][2];
+    mat[val][5] = mat[val][3] - mat[val][1];
+    mat[val][4] = mat[val][5] - mat[val][2];
+    for (let k = 0; k < 6; k++) {
+      let tem = mat[val][k];
+      mat[val][k] = mat[i][k];
+      mat[i][k] = tem;
+    }
+  }
+  //BackEnd Ends here
 
-    var Table = document.getElementById("ljfTable");
-    var rows = Table.querySelectorAll('tr');
+  var Table = document.getElementById("ljfTable");
+  var rows = Table.querySelectorAll("tr");
+  for (let i = 1; i < rows.length; i++) {
+    rows[i].children[0].innerHTML =
+      '<input id="lpid' +
+      i +
+      '" placeholder="pid' +
+      i +
+      '" value=' +
+      mat[i - 1][0] +
+      ">";
+    rows[i].children[1].innerHTML =
+      '<input id="lat' +
+      i +
+      '" placeholder="at' +
+      i +
+      '" value=' +
+      mat[i - 1][1] +
+      ">";
+    rows[i].children[2].innerHTML =
+      '<input id="lbt' +
+      i +
+      '" placeholder="bt' +
+      i +
+      '" value=' +
+      mat[i - 1][2] +
+      ">";
+    rows[i].children[3].innerHTML = mat[i - 1][3];
+    rows[i].children[4].innerHTML = mat[i - 1][5];
+    rows[i].children[5].innerHTML = mat[i - 1][4];
+  }
+
+  google.charts.load("current", { packages: ["timeline"] });
+
+  const drawTimeLine = () => {
+    var container = document.getElementById("LJFtimeline");
+    var chart = new google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
+
+    dataTable.addColumn({ type: "string", id: "Process" });
+    dataTable.addColumn({ type: "number", id: "Start" });
+    dataTable.addColumn({ type: "number", id: "End" });
+
+    let arr = [];
+
     for (let i = 1; i < rows.length; i++) {
-        rows[i].children[0].innerHTML = '<input id="lpid' + i + '" placeholder="pid' + i + '" value=' + mat[i - 1][0] + '>';
-        rows[i].children[1].innerHTML = '<input id="lat' + i + '" placeholder="at' + i + '" value=' + mat[i - 1][1] + '>';
-        rows[i].children[2].innerHTML = '<input id="lbt' + i + '" placeholder="bt' + i + '" value=' + mat[i - 1][2] + '>';
-        rows[i].children[3].innerHTML = mat[i - 1][3];
-        rows[i].children[4].innerHTML = mat[i - 1][5];
-        rows[i].children[5].innerHTML = mat[i - 1][4];
+      // console.log(AT[i-1], wt[i-1]);
+      // console.log(ct[i-1]);
+      arr.push([
+        "P" + i,
+        (mat[i - 1][1] + mat[i - 1][4]) * 1000,
+        mat[i - 1][3] * 1000,
+      ]);
     }
+    dataTable.addRows(arr);
+    chart.draw(dataTable);
+  };
+  google.charts.setOnLoadCallback(drawTimeLine);
+  const drawGantt = () => {
+    var container = document.getElementById("LJFgantt");
+    var chart = new google.visualization.Timeline(container);
+    var dataTable = new google.visualization.DataTable();
 
-    google.charts.load('current', { 'packages': ['timeline'] });
+    dataTable.addColumn({ type: "string", id: "Position" });
+    dataTable.addColumn({ type: "string", id: "Process" });
+    dataTable.addColumn({ type: "number", id: "Start" });
+    dataTable.addColumn({ type: "number", id: "End" });
 
-    const drawTimeLine = () => {
-        var container = document.getElementById('LJFtimeline');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
+    let arr = [];
 
-        dataTable.addColumn({ type: 'string', id: 'Process' });
-        dataTable.addColumn({ type: 'number', id: 'Start' });
-        dataTable.addColumn({ type: 'number', id: 'End' });
-
-        let arr = [];
-
-        for (let i = 1; i < rows.length; i++) {
-            // console.log(AT[i-1], wt[i-1]);
-            // console.log(ct[i-1]);
-            arr.push([
-                'P' + i,
-                (mat[i - 1][1] + mat[i - 1][4]) * 1000,
-                (mat[i - 1][3]) * 1000
-            ])
-        }
-        dataTable.addRows(arr);
-        chart.draw(dataTable);
+    for (let i = 1; i < rows.length; i++) {
+      // console.log(AT[i-1], wt[i-1]);
+      // console.log(ct[i-1]);
+      arr.push([
+        "Process",
+        "P" + i,
+        (mat[i - 1][1] + mat[i - 1][4]) * 1000,
+        mat[i - 1][3] * 1000,
+      ]);
     }
-    google.charts.setOnLoadCallback(drawTimeLine);
-    const drawGantt = () => {
-        var container = document.getElementById('LJFgantt');
-        var chart = new google.visualization.Timeline(container);
-        var dataTable = new google.visualization.DataTable();
-
-        dataTable.addColumn({ type: 'string', id: 'Position' });
-        dataTable.addColumn({ type: 'string', id: 'Process' });
-        dataTable.addColumn({ type: 'number', id: 'Start' });
-        dataTable.addColumn({ type: 'number', id: 'End' });
-
-        let arr = [];
-
-        for (let i = 1; i < rows.length; i++) {
-            // console.log(AT[i-1], wt[i-1]);
-            // console.log(ct[i-1]);
-            arr.push([
-                'Process',
-                'P' + i,
-                (mat[i - 1][1] + mat[i - 1][4]) * 1000,
-                (mat[i - 1][3]) * 1000
-            ])
-        }
-        dataTable.addRows(arr);
-        chart.draw(dataTable);
-    }
-    google.charts.setOnLoadCallback(drawGantt);
+    dataTable.addRows(arr);
+    chart.draw(dataTable);
+  };
+  google.charts.setOnLoadCallback(drawGantt);
 }
 var LJFcounter = 2;
 function LJFfn() {
-    let mat = [];
-    for (let i = 1; i < LJFcounter; i++) {
-        if (document.getElementById('lat' + i) !== undefined && document.getElementById('lbt' + i) !== undefined) {
-            console.log(i);
-            // mat[i][0] = i;
-            mat.push([]);
-            mat[i - 1][0] = (parseInt(document.getElementById("lpid" + (i)).value));
-            mat[i - 1][1] = (parseInt(document.getElementById("lat" + (i)).value));
-            mat[i - 1][2] = (parseInt(document.getElementById("lbt" + (i)).value));
-        }
+  let mat = [];
+  for (let i = 1; i < LJFcounter; i++) {
+    if (
+      document.getElementById("lat" + i) !== undefined &&
+      document.getElementById("lbt" + i) !== undefined
+    ) {
+      console.log(i);
+      // mat[i][0] = i;
+      mat.push([]);
+      mat[i - 1][0] = parseInt(document.getElementById("lpid" + i).value);
+      mat[i - 1][1] = parseInt(document.getElementById("lat" + i).value);
+      mat[i - 1][2] = parseInt(document.getElementById("lbt" + i).value);
     }
-    // console.log(mat.length);
-    LJF(LJFcounter, mat);
+  }
+  // console.log(mat.length);
+  LJF(LJFcounter, mat);
 }
 
 function addLJFRow() {
-    $('#ljfTable').append('<tr><td><input disabled type="number" id="lpid' + LJFcounter + '" placeholder="pid' + LJFcounter + '" value="' + LJFcounter + '"/></td><td><input type="number" id="lat' + LJFcounter + '" placeholder="at' + LJFcounter + '" /></td><td><input type="number" id="lbt' + LJFcounter + '" placeholder="bt' + LJFcounter + '" /></td><td></td><td></td><td></td></tr>');
-    LJFcounter += 1;
+  $("#ljfTable").append(
+    '<tr><td><input disabled type="number" id="lpid' +
+      LJFcounter +
+      '" placeholder="pid' +
+      LJFcounter +
+      '" value="' +
+      LJFcounter +
+      '"/></td><td><input type="number" id="lat' +
+      LJFcounter +
+      '" placeholder="at' +
+      LJFcounter +
+      '" /></td><td><input type="number" id="lbt' +
+      LJFcounter +
+      '" placeholder="bt' +
+      LJFcounter +
+      '" /></td><td></td><td></td><td></td></tr>'
+  );
+  LJFcounter += 1;
 }
 
 function deleteLJFRow() {
-    var table = document.getElementById("ljfTable");
-    var rowCount = table.rows.length;
+  var table = document.getElementById("ljfTable");
+  var rowCount = table.rows.length;
 
-    if(rowCount > 2) {
-        table.deleteRow(rowCount - 1);
-        LJFcounter -= 1;
-    }
-    else {
-        alert("Cannot delete row");
-    }
+  if (rowCount > 2) {
+    table.deleteRow(rowCount - 1);
+    LJFcounter -= 1;
+  } else {
+    alert("Cannot delete row");
+  }
 }
