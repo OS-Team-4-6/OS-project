060ab0a68c975296dc414de9bd874e793f29533a
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-03-22 00:00:34 +0530

    Changed developers.php

--- developers.php
+++ developers.php
@@ -1,199 +1,220 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="./css/style.css" />
-        <link rel="stylesheet" href="./css/tabs.css" />
-        <link rel="stylesheet" href="./css/person-card.css" />
-        <title>Developers</title>
-        <script src="./js/tabs.js"></script>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/tabs.css" />
+    <link rel="stylesheet" href="./css/header.css" />
+    <link rel="stylesheet" href="./css/person-card.css" />
+    <title>Developers</title>
+    <script src="./js/tabs.js"></script>
+</head>
 
-    <body>
-        <?php
-    $title_1 = "";
-    $title_2 = "Developers";
-    $main_desc = "";
+<body>
+    <!-- <?php
+            $title_1 = "";
+            $title_2 = "Developers";
+            $main_desc = "";
 
-    include "header.php";
-    ?>
+            include "header.php";
+            ?> -->
 
-        <div class="tabs-container flex-layout ">
-            <div data-tab="disk" class="dev tab h4 active-tab flex-layout justify-center items-center text-uppercase">
-                Disk Scheduling
+    <div class="landing-screen-2">
+        <nav>
+            <div class="navbar flex-layout justify-center items-center white-text text-uppercase">
+                <div class="nav-items"><a href="./index.php">Home</a></div>
+                <div class="nav-items"><a href="./Main.php">Disk Scheduling</a></div>
+                <div class="nav-items"><a href="./pageReplacementAlgo.php">Page Replacement</a></div>
+                <div class="nav-items"><a href="./schedulingAlgo.php">Scheduling</a></div>
+                <div class="nav-items"><a href="">Concurrency & Deadlock</a></div>
+                <div class="nav-items"><a href="./developers.php">about</a></div>
             </div>
-            <div data-tab="scheduling" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-                Scheduling
+        </nav>
+
+        <div class="header-container-2 flex-layout justify-center items-center">
+            <h1 class="header-title bold-text white-text">Developers</h1>
+        </div>
+    </div>
+
+    <div class="tabs-container flex-layout ">
+        <div data-tab="scheduling" class="dev tab h4 active-tab flex-layout justify-center items-center text-uppercase">
+            Scheduling
+        </div>
+        <div data-tab="pageRep" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+            Page Replacement
+        </div>
+        <div data-tab="disk" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+            Disk Scheduling
+        </div>
+        <div data-tab="concurrency" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
+            Concurrency & Deadlock
+        </div>
+    </div>
+
+    <div id="disk" class="tab-view">
+        <section class="person-cards">
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Lorem Ipsum</h4>
+                    <h5 class="person-card-des">President</h5>
+                </div>
             </div>
-            <div data-tab="pageRep" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-                Page Replacement
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Lorem Ipsum</h4>
+                    <h5 class="person-card-des">President</h5>
+                </div>
             </div>
-            <div data-tab="concurrency" class="dev tab h4 flex-layout justify-center items-center text-uppercase">
-                Concurrency & Deadlock
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Lorem Ipsum</h4>
+                    <h5 class="person-card-des">President</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Lorem Ipsum</h4>
+                    <h5 class="person-card-des">President</h5>
+                </div>
+            </div>
+
+        </section>
+    </div>
+    <div id="scheduling" class="tab-view">
+        <section class="person-cards">
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/nehaBadiani.JPG" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Neha Badiani</h4>
+                    <h5 class="person-card-des">Team Lead</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/divijShah.jpeg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Divij Shah</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/rajviShah.jpeg" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Rajvi Shah</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/rushabhShah.JPG" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Rushabh Shah</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/vedantPatel.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Vedant Patel</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+        </section>
+    </div>
+    <div id="pageRep" class="tab-view">
+        <section class="person-cards">
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/smitVekaria.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Smit Vekaria</h4>
+                    <h5 class="person-card-des">Team Lead</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/atharvaPatel.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Atharva Patel</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/maitriSurti.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Maitri Surti</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/shramKadia.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Shram Kadia</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+
+            <div class="person-card white-text">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/vyomShah.png" alt="Person Image">
+                </div>
+                <div class="person-card-body">
+                    <h4 class="person-card-name">Vyom Shah</h4>
+                    <h5 class="person-card-des">Member</h5>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div id="concurrency" class="tab-view">
+        <div class="empty flex-layout justify-center items-center">
+            <div class="h1 white-text">
+                Not available
             </div>
         </div>
-
-        <div id="disk" class="tab-view">
-            <section class="person-cards">
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Lorem Ipsum</h4>
-                        <h5 class="person-card-des">President</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Lorem Ipsum</h4>
-                        <h5 class="person-card-des">President</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Lorem Ipsum</h4>
-                        <h5 class="person-card-des">President</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/post.jpg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Lorem Ipsum</h4>
-                        <h5 class="person-card-des">President</h5>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-        <div id="scheduling" class="tab-view">
-            <section class="person-cards">
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/nehaBadiani.JPG" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Neha Badiani</h4>
-                        <h5 class="person-card-des">Team Lead</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/divijShah.jpeg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Divij Shah</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/rajviShah.jpeg" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Rajvi Shah</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/rushabhShah.JPG" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Rushabh Shah</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/vedantPatel.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Vedant Patel</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-        <div id="pageRep" class="tab-view">
-            <section class="person-cards">
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/smitVekaria.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Smit Vekaria</h4>
-                        <h5 class="person-card-des">Team Lead</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/atharvaPatel.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Atharva Patel</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/maitriSurti.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Maitri Surti</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/shramKadia.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Shram Kadia</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-                <div class="person-card white-text">
-                    <div class="person-card-header">
-                        <img class="person-card-img" src="./assets/images/vyomShah.png" alt="Person Image">
-                    </div>
-                    <div class="person-card-body">
-                        <h4 class="person-card-name">Vyom Shah</h4>
-                        <h5 class="person-card-des">Member</h5>
-                    </div>
-                </div>
-
-            </section>
-        </div>
-        <div id="concurrency" class="tab-view">
-
-        </div>
-    </body>
+    </div>
+</body>
 
 </html>
