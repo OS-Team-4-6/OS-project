022cc73d049b47243fbda80d4f68c6726ccfc015
Smit Vekaria <smit.vce19@sot.pdpu.ac.in>
2021-04-05 20:16:01 +0530

    Updated index.php

--- index.php
+++ index.php
@@ -6,20 +6,75 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/person-card.css">
     <title>Homepage</title>
+
+    <style>
+        .index:hover img {
+            box-shadow:5px 5px 10px #000;
+        }
+
+        .person-card-img {
+            width: 100%;
+        }
+    </style>
+
 </head>
 <body>
-    <div id="disk-scheduling" class="home-cards">
-        <a href="./Main.php"><h4>Disk Scheduling Algorithms</h4></a>
-    </div>
-    <div id="scheduling" class="home-cards">
-        <a href="./schedulingAlgo.php"><h4>Scheduling Algorithms</h4></a>
-    </div>
-    <div id="page-replacement" class="home-cards">
-        <a href="./pageReplacementAlgo.php"><h4>Page Replacement Algorithms</h4></a>
-    </div>
-    <div id="concurrency-deadlock" class="home-cards">
-        <a href=""><h4>Concurrency and Deadlock</h4></a>
-    </div>
+    <?php 
+        $title_1 = "OS";
+        $title_2 = "Virtual Lab";
+        $main_desc = "";
+        include 'header.php'; 
+    ?>
+
+    <section class="person-cards">
+
+        <div class="index person-card white-text items-center justify-center">
+            <a href="./Main.php">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/hard-disk1.jpg" alt="Person Image">
+                </div>
+            </a>
+            <div class="person-card-body">
+                <h3 class="person-card-name text-center">Disk Scheduling<br>Algorithms</h3>
+            </div>
+        </div>
+
+        <div class="index person-card white-text items-center justify-center">
+            <a href="./schedulingAlgo.php">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/scheduling-bg.jpg" alt="Person Image">
+                </div>
+            </a>
+            <div class="person-card-body">
+                <h3 class="person-card-name text-center">Scheduling Algorithms</h3>
+            </div>
+        </div>
+
+        <div class="index person-card white-text items-center justify-center">
+            <a href="./pageReplacementAlgo.php">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/page-replacement-bg.jpg" alt="Person Image">
+                </div>
+            </a>
+            <div class="person-card-body">
+                <h3 class="person-card-name text-center">Page Replacement<br>Algorithms</h3>
+            </div>
+        </div>
+
+        <div class="index person-card white-text items-center justify-center">
+            <a href="#">
+                <div class="person-card-header">
+                    <img class="person-card-img" src="./assets/images/concurrency-deadlock-bg.jpg" alt="Person Image">
+                </div>
+            </a>
+            <div class="person-card-body">
+                <h3 class="person-card-name text-center">Concurrency &<br>Deadlock Algorithms</h3>
+            </div>
+        </div>
+
+    </section>
+
 </body>
 </html>
